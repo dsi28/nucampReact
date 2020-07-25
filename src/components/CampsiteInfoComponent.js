73bad854ab13af0cb4b204f01498de9e261d33c0
David <dsi28@hotmail.com>
2020-07-24 23:37:11 -0500

    add react animations

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -7,6 +7,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const minLength = len => val => val && (val.length >= len);
 const maxLength = len => val => !val || (val.length <= len);
@@ -17,15 +18,18 @@ function RenderComments({comments, postComment, campsiteId}){
         return (
             <div className="col-md-5 m-1">
                 <h4>Comments</h4>
-
+                <Stagger in>
                 {comments.map(comment => {
                     return (
+                        <Fade in key={comment.id}>
                         <> {/* react basic container? */}
                             <p>{comment.text}</p>
                             <p>{comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                         </>
+                        </Fade>
                     )
                 })}
+                </Stagger>
                 <CommentForm 
                     campsiteId={campsiteId} 
                     postComment={postComment}/>
@@ -134,12 +138,18 @@ class CommentForm extends Component{
 function RenderCampsite({campsite}) {
     return (
         <div className="col-md-5 m-1">
-            <Card>
-                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
-                <CardBody>
-                    <CardText>{campsite.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+            }}>
+                <Card>
+                    <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         </div>
     )
 }
